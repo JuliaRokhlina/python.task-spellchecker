a9a6d22b3af04395efbf05bdccd022c85bef27e0
JuliaRokhlina <92213643+JuliaRokhlina@users.noreply.github.com>
2022-07-28 15:39:23 +0500

    tests

--- dictionary builder.py
+++ dictionary builder.py
@@ -1,16 +1,23 @@
 import regex as re
 
-dictionary = set([w for s in open("dictionary.txt", encoding='utf-8', newline='').readlines() for w in s.split()])
-special = re.compile("[,.?/…!\"\'*–();:«»1234567890\[\]]")
+dictionary = dict()
+for line in open("dictionary.txt", encoding='utf-8', newline='').readlines():
+    line_pair = line.split()
+    if len(line_pair) == 2:
+        dictionary[line_pair[0]] = int(line_pair[1])
+special = re.compile("[,.?/…!\"\'*–—();:«»„“1234567890\[\]]")
 
 
 def add_words(collection):
-    dict_file = open("dictionary.txt", 'a', encoding='utf-8')
+    dict_file = open("dictionary.txt", 'r+', encoding='utf-8')
     for word in collection:
         word_fixed = re.sub(special, "", word).lower()
-        if word_fixed not in dictionary:
-            dictionary.add(word_fixed)
-            dict_file.write(word_fixed + "\n")
+        if word_fixed in dictionary:
+            dictionary[word_fixed] += 1
+        else:
+            dictionary[word_fixed] = 1
+    for pair in sorted(dictionary.items(), key=lambda item: -item[1]):
+        dict_file.write(pair[0] + " " + str(pair[1]) + "\n")
     dict_file.close()
 
 
