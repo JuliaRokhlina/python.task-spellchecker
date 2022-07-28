f2ef9d85d90b90af5e5c77f69e57d9c901dd397b
JuliaRokhlina <92213643+JuliaRokhlina@users.noreply.github.com>
2022-07-28 19:31:50 +0500

    (3/4)final

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ import spellchecker
 checker = spellchecker.SpellChecker()
 print("Введите текст без знаков препинания и заглавных букв:")
 s = input()
-checker.spell_check(s)
+print("Введите, сколько первых ошибок найти (введите '-', если все)")
+n = int(input())
+checker.spell_check(s, n)
